0ccc971ee6d0aa9330bfdc36d60fb8a60a5fc4a5
Agustin Bilbao <agustinbil95@gmail.com>
2023-05-09 13:33:01 -0300

    Axios implementado

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 
   
   
-   const API_DEALS = "https://www.cheapshark.com/api/1.0/deals?storeID=1";
+  /*  const API_DEALS = "https://www.cheapshark.com/api/1.0/deals?storeID=1";
     
     async function getDeals (URL){
         const res = await fetch(URL, {
@@ -70,9 +70,90 @@
         
     }
 
-    getDeals(API_DEALS);
+    getDeals(API_DEALS); */
 
     
    
+   
 
+   const api = axios.create({
+    method: 'get',
+    maxBodyLength: Infinity,
+    baseURL: 'https://www.cheapshark.com/api/1.0',
+    headers: { }
+   })
+    
+   async function getDeals(storeID){
+    const res = await api('/deals?storeID=' + storeID);
+    const sales = res.data;
+    
+    console.log(sales)
+     
+    sales.forEach(game => {
+        const dealContainer = document.createElement('div');
+        const gameLink = document.createElement('a');
+        const img = document.createElement('img');
+        const detContainer = document.createElement('div');
+        const title = document.createElement('h3');
+        const prContainer = document.createElement('div');
+        const dealPrice = document.createElement('p');
+        const price = document.createElement('p');
+        const saving = document.createElement('p');
+        const steamRating = document.createElement('p');
+
+        
+        
+    
+        dealContainer.classList.add('deal-container');
+        gameLink.classList.add('game-link');
+        
+        gameLink.setAttribute('target', "_blank")
+        img.setAttribute('src', game.thumb);
+        img.classList.add('game-image');
+        detContainer.classList.add('details-container')
+        title.classList.add('game-title');
+        title.innerHTML = game.title;
+        prContainer.classList.add('prices-container');
+        dealPrice.classList.add('deal-price')
+        dealPrice.innerHTML = '$ ' + game.salePrice;
+        price.classList.add('original-price');
+        price.innerHTML =  '$ ' + game.normalPrice;
+        saving.classList.add('saving');
+        saving.innerHTML = '-' + Math.trunc(game.savings) + '%';
+        steamRating.classList.add('steam-rating');
+        steamRating.innerHTML = 'Steam rating: ' + game.steamRatingText;
+        gameLink.setAttribute('href', 'https://cheapshark.com/redirect?dealID=' + game.dealID )
+      
+        document.body.appendChild(dealContainer);
+        dealContainer.appendChild(gameLink);
+        gameLink.appendChild(img);
+        dealContainer.appendChild(detContainer);
+        detContainer.appendChild(title);
+        detContainer.appendChild(prContainer);
+        detContainer.appendChild(steamRating);
+        prContainer.appendChild(dealPrice);
+        prContainer.appendChild(price);
+        prContainer.appendChild(saving);
+    
+    
+    
+    
+    
+    
+    
+    });
+    
+
+    
+   }
+
+
+   
+    
+   
+
+
+   
+   
+    
     
